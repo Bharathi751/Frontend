4b737cc0ed2561bd8d1013570d643bdedff7a6d7
Bharathi751 <itismebharathi@gmail.com>
2026-08-19 16:50:43 +0530

    day2 completed

---
+++ JavaScript/Day2/task/assets/js/task.js
@@ -0,0 +1,14 @@
+var studentName="Bharathi"
+console.log(studentName);
+
+var age=22
+console.log(age);
+
+var course="BCA"
+console.log(course);
+
+var fees=55499
+console.log(fees)
+
+var isPlaced=true
+console.log(isPlaced)
