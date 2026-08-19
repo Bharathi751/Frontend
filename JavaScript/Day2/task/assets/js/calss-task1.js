d73f758c5071b16da2c8955d214a7878cbba8dae
Bharathi751 <itismebharathi@gmail.com>
2026-08-19 21:54:14 +0530

    day 2 completed

---
+++ JavaScript/Day2/task/assets/js/calss-task1.js
@@ -0,0 +1,81 @@
+/*  var use in string 18 times*/
+
+var userName="Baharathi"
+var studentName="Vicky"
+var employeName="Tamil"
+var employeID="112233"
+var houseAddress="Chennai"
+var section="Bsection"
+var emailAddress="bharathi@gamil"
+var collegeName="Alpha"
+var batchNumber="Two"
+var bloodGroup="O+"
+var age="33"
+var location="Kerala"
+var workPlace="Dubai"
+var positiveEnergy="Polite"
+var loacalHost="http"
+var machineLearning="Alogorithm"
+var schoolBags="AmericanTourister"
+var keyWord="MainWord"
+var plugin="sound"
+
+
+/*  let use in number 18 times*/
+
+let ratio=5.1
+let addition=5+1
+let subtraction=5-
+let division=6/
+let mutiple=8*
+let percentage=10%
+let decimal=66.6
+let number=123
+let one=1
+let arrange=112233
+let average=2468
+let sum=56+
+let product=65
+let primeValue=123456
+let oddOne=3579
+let evenOne=2468
+let root=2416
+let cube=3981
+
+/*  true use in 10 times */
+
+const vegitableGreen=true
+const single=true
+const mariied=true
+const fakeOne=true
+const groupOne=true
+const separateOne=true
+const valuedOne=true
+const statement=true
+const variable=true
+const contant=true
+
+/*  false use in 10 times */
+
+const outof=false
+const oddOne=false
+const group=false
+const elements=false
+const variables=false
+const declaare=false
+const highValue=false
+const single=false
+const tables=false
+const combunation=false
+
+/* undefined use in 3 times */
+
+var bigValue
+let smallOne
+const middleOne
+
+/* null use in 3 times */
+
+var containerElement=null
+let parts=null
+const cardElement=null
---
+++ JavaScript/Day2/task/assets/js/class-task2.js
@@ -0,0 +1,54 @@
+
+/* console.log(); string 10 times */
+
+console.log("userName");
+console.log("Phone");
+console.log("email");
+console.log("main");
+console.log("variable");
+console.log("contant");
+console.log("IDname");
+console.log("number");
+console.log("alphabet");
+console.log("columnValue");
+
+/* console.log(); string 10 times */
+
+console.log(123);
+console.log(456);
+console.log(789);
+console.log(0123);
+console.log(0456);
+console.log(0789);
+console.log(0987);
+console.log(0654);
+console.log(0321);
+console.log(02468);
+
+/* console.log(); true 5 times */
+
+console.log(true);
+console.log(true);
+console.log(true);
+console.log(true);
+console.log(true);
+
+/* console.log(); false 5 times */
+
+console.log(false);
+console.log(false);
+console.log(false);
+console.log(false);
+console.log(false);
+
+/* console.log(); undefined 3 times */
+
+console.log(undefined);
+console.log(undefined);
+console.log(undefined);
+
+/* console.log(); null 3 times */
+
+console.log(null);
+console.log(null);
+console.log(null);
