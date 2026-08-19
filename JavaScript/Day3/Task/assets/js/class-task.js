ea0b2feb5f98fb6d69c79787c693b22e9462c6f1
Bharathi751 <itismebharathi@gmail.com>
2026-08-20 01:08:20 +0530

    class task completed

---
+++ JavaScript/Day3/Task/assets/js/class-task.js
@@ -0,0 +1,94 @@
+let name="Vicky"
+name="BHarathi"
+console.log(name);
+
+let officeName="TCS"
+officeName="HCL"
+console.log(officeName);
+
+let shoppingMall="SaravanaStore"
+shoppingMall="ChennaiSilks"
+console.log(shoppingMall);
+
+let place="kerala"
+place="munnar"
+console.log(place);
+
+let food="Biriyani"
+food="FriedRice"
+console.log(food);
+
+let brand="puma"
+brand="nike"
+console.log(brand);
+
+let paint="white"
+paint="black"
+console.log(paint);
+
+let color="gradient"
+color="silavet"
+console.log(color);
+
+let odd=3
+odd=5
+console.log(odd);
+
+let even=4
+even=8
+console.log(even);
+
+let discount=50%
+discount=80%
+console.log(discount);
+
+let state="TamilNadu"
+state="Karnataka"
+console.log(state);
+
+let ocean="bay of bengal"
+ocean="pacific ocean"
+console.log(ocean);
+
+let beach="marina"
+beach="adayar"
+console.log(beach);
+
+let game="hockey"
+game="football"
+console.log(game);
+
+let bike="pulsar"
+bike="duke390"
+console.log(bike);
+
+let launge="english"
+launge="tamil"
+console.log(launge);
+
+let story="love"
+story="horror"
+console.log(story);
+
+let movieGener="Fantasy"
+movieGener="RomanticComedy"
+console.log(movieGener);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
