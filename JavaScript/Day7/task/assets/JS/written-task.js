e9dab859a8a57e59dd935ae181e61b74dc869905
Bharathi751 <itismebharathi@gmail.com>
2026-08-26 02:04:55 +0530

    day 7 completed

---
+++ JavaScript/Day7/task/assets/JS/written-task.js
@@ -0,0 +1,290 @@
+// Array 48 times.
+
+
+let fruits=[ "Apple","Orange" ];
+console.log(fruits [1]);
+
+let age=[1,2,3,4,];
+console.log( age[2]);
+
+
+let car=[ brand1, brand2, brand3];
+console.log(car [2]);
+
+
+let even=[ 2,4,6,8];
+console.log( even[3]);
+
+let odd=[ 1,3,7,5,9 ];
+console.log( odd[4]);
+
+
+let alphabet=["A","B","C","D" ];
+console.log(alphabet [2]);
+
+
+let bag=[1,2,3,4,5, ];
+console.log(bag [3]);
+
+
+let games=[ "GTA","RDR","EVIL" ];
+console.log( games[0]);
+
+
+let course=["HTML" "JS", ];
+console.log(course [1]);
+
+
+let anime=["AOT","1Pice" ];
+console.log( anime[0]);
+
+
+let class=[1,2,3,4,5,6 ];
+console.log(class [5]);
+
+
+let object=[Fan ,Table ];
+console.log( object[1]);
+
+
+let sowftware =[ AI,ML ];
+console.log(sowftware []);
+
+
+let machine=[ Testing,Machine ];
+console.log(machine [0]);
+
+
+let bike=[ "apache","tvs" ];
+console.log(bike [1]);
+
+
+let phones=[ "apple","android" ];
+console.log(phones [0]);
+
+
+let cells=[blood,neverse ];
+console.log(cells [0]);
+
+
+let product=[ laptop,tablet ];
+console.log(product [0]);
+
+
+let forest=["green","yellow" ];
+console.log( forest[1]);
+
+
+let colour=[white ,black ];
+console.log(colour [1]);
+
+
+let movie=["hollywood" ,"kollywood" ];
+console.log( movie[0]);
+
+
+let hero=[ "Ajith","Vijay" ];
+console.log(hero [1]);
+
+let book=[ Tamil,English];
+console.log( book[1]);
+
+let animal=[ tiger,lion ];
+console.log( animal[1]);
+
+let task=[oneT,TwoT,ThreeT ];
+console.log( task[2]);
+
+let score=[ 55,9,20,96];
+console.log(score [3]);
+
+let city=[chennai,T.nagar ];
+console.log( city[]);
+
+let player=[ P1,P2,P3,P4 ];
+console.log( player[2]);
+
+let decimal=[ 0.5,0.9 ,0.99];
+console.log( decimal[1]);
+
+let school=["Tamil","English" ];
+console.log(school [0]);
+
+let add=[ 2,3,5,10,15,25 ];
+console.log( add[5]);
+
+let net=[online ,offline ];
+console.log( net[]);
+
+let parts=[1,2,3,4 ];
+console.log(parts [3]);
+
+let paint=["orange","green","blue"];
+console.log(paint [2]);
+
+let tool=[search,AI,ML];
+console.log( tool[2]);
+
+let border=[ "margin","padding",box ];
+console.log(border [2]);
+
+let vechile=[ bike,scooty];
+console.log(vechile [1]);
+
+let app=[ game,media,camera ];
+console.log( app[2]);
+
+let state=["tamilnadu","kerala" ];
+console.log(state[1]);
+
+let friend=["Tamil","Vicky","akash" ];
+console.log(friend [2]);
+
+let EID=[ EID4,Eid8];
+console.log( EID[0]);
+
+let names=[ "Name1",Name2,Name3];
+console.log(names [1);
+
+let primes=[2,3,4,5,6,7 ];
+console.log( primes[5]);
+
+let pets=[ "cat","dog", birds ];
+console.log( pets[]);
+
+let address=[ ADI,AD2,AD3 ];
+console.log( address[2]);
+
+let sports=[ Cricket,"foot abull" ];
+console.log( sports[0]);
+
+
+
+
+
+
+// objects in array
+
+
+let student = {names : Bharathi, age :20 }
+console.log(student.names);
+
+let employee = {names : "tamil", ID :2026 }
+console.log(employee.names);
+
+let car = {brand :Audi , price :50,00,000 }
+console.log(car.brand);
+
+let bike = {brand :"yamaha" , model :R15 }
+console.log(bike.model);
+
+let phone = {model : S"24Ultra", brand :"Samsung" }
+console.log(phone.brand);
+
+let laptop = {brand :"HP" , model :"HP4689" }
+console.log(laptop.brand);
+
+let fruit = {names : "Apple", colour :"red" }
+console.log(fruit.colour);
+
+let vegitable = {names : "carrot", colour :"orange" }
+console.log(vegitable.names);
+
+let camera = {model :"5Dmark4" , brand : "canon"}
+console.log(camera.model);
+
+let game = {names : "GTA", gener : "gangster"}
+console.log(game.names);
+
+let movie = {names :"leo" , actor : "vijay"}
+console.log(movie.actor);
+
+let colour = {names :"red" , about :"emotion" }
+console.log(colour.about);
+
+let pencil = {brand :"apsara" , pricr : 10}
+console.log(pencil.brand);
+
+let state = {names :"tamilnadu" , population :5,00,000 }
+console.log(state.names);
+
+let ticket = {price :499 , show : "comedy"}
+console.log(ticket.show);
+
+let park = {names : "towerpark", place :"anna nagar" }
+console.log(park.place);
+
+let beach = {names : "bessy", palce : "adayar"}
+console.log(beach.names);
+
+let launge = {names : "tamil", letters : 247}
+console.log(launge.names);
+
+let hollywood = {movie : "x-men", gener :"Thriller" }
+console.log(hollywood.movie);
+
+let watch = {model : "I10", brand : "apple"}
+console.log(watch.brand);
+
+let website = {names :"google" , url :"google.com" }
+console.log(website.names);
+
+let player = {names : "dhoni", number : 7}
+console.log(player.names);
+
+let friend = {names : "tamil", age : 24}
+console.log(friend.age);
+
+let Tamilselvan = {gender :"male" , height : "175cm"}
+console.log(Tamilselvan.gender);
+
+let sports = {names :"football" , game : "outdoor"}
+console.log(sports.names);
+
+let television = {brand :"LG" , model : "LG35R7Z"}
+console.log(television.brand);
+
+let shirt = {size : "48cm", brand : "allensolly"}
+console.log(shirt.brand);
+
+let storage = {type : "memory", space :"512gb" }
+console.log(storage.type);
+
+let nature = {place :"ooty" , distance : "580km"}
+console.log(nature.place);
+
+let number = {type :"odd" , value : 7}
+console.log(number.value);
+
+let girl = {names : "sarah", age : "19"}
+console.log(girl.names);
+
+let weather = {place : "chennai", temperature : "51"}
+console.log(weather.place);
+
+let flower = {names : "rose", colour :"red" }
+console.log(flower.names);
+
+let pets = {names : "dog", age :5 }
+console.log(pet.names);
+
+let subject = {names :"DM" , marks : "99"}
+console.log(subject.);
+
+let song = {names :"let her go" , singer :"ed shareen" }
+console.log(song.names);
+
+let devoloper = {names : "vicky", exprience : "3years"}
+console.log(devoloper.);
+
+let testor = {names :"akash" , age : 22}
+console.log(testor.names);
+
+let actor = {names :"vijay" , age : 52}
+console.log(actor.names);
+
+let bird = {names :"parrot" , colour : "green"}
+console.log(bird.colour);
+
+let juice = {names :"orange" , benifit :"Vitmin C" }
+console.log(juice.names);
