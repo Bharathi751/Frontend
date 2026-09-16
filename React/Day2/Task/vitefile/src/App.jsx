6a3be7607fc87cdb8bb3591e09c98bef007f066e
Bharathi751 <itismebharathi@gmail.com>
2026-09-16 12:16:48 +0530

    day 1 completed

---
+++ React/Day2/Task/vitefile/src/App.jsx
@@ -0,0 +1,23 @@
+const App = ()=>{
+  return(<>
+  
+  <div> The React  </div>
+  
+  
+  
+  
+  </>)
+};
+export default App
+
+export const Name = ()=>{
+  return(<>
+  
+  
+    <button class="style"> Click Name</button>
+
+  
+  </>)
+}
+import './App.css'
+
